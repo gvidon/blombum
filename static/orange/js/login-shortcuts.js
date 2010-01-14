2dacfb4104958f1bc6c3fc0a2cd1bce9fa389dd9
nop <nop@castor.canis.canis>
2010-01-14 22:45:09 +0300

    login form fix

--- static/orange/js/login-shortcuts.js
+++ static/orange/js/login-shortcuts.js
@@ -1,22 +1,13 @@
-function form_close() {
-	$("#login-form").hide();
-	$("#openid-form").hide();
-	$("#login-close").hide();
-    return false;
-}
-
 function show_login() {
 	$("#login-form").show();
 	$("#openid-form").hide();
-	$("#login-close").show();
-    $("#login-form #email").focus();
-    return false;
+	$("#email").focus();
+	return false;
 }
 
 function show_openid() {
 	$("#login-form").hide();
 	$("#openid-form").show();
-	$("#login-close").show();
-    $("#openid-form #id_openid_url").focus();
-    return false;
+	$("#id_openid_url").focus();
+	return false;
 }
