6a9a6048edb4f48012db3b75ff5aa0c5c32a5c5c
Gvidon <nide@inbox.ru>
2010-02-06 21:47:12 +0300

    static pages

---
+++ static/js/crossposting.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	/*
+	$('#id_crosspost_que').hide();
+	
+	$('#id_crosspost_que').after('<a class="jqModal" href="#">text</a>');
+	$('#id_crosspost_que').after('<div id="asd" class="jqmWindow"><div class="jqmTitle">Crossposting</div><div class="jqmClose"><a href="#">закрыть</a></div>asdasd</div>');
+	
+	$('#asd').jqm();
+	*/
+});
