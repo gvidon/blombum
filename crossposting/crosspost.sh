1857f48e07c32c4f7ecf9663849b90337f15d2be
Gvidon <nide@inbox.ru>
2010-02-10 00:20:02 +0300

    cleaned up crossposting/ dir

--- crossposting/crosspost.sh
+++ crossposting/crosspost.sh
@@ -3,7 +3,7 @@
 echo "[
 	{'crawler': 'blogger', 'params': {
 		'email'   : 'nenegoro@gmail.com',
-		'password': 'zinaida123097',
+		'password': '',
 		'blogid'  : '2590107552820081312',
 		'body'    : 'пост ',
 		'tags'    : 'тэг, еще, третий',
@@ -11,7 +11,7 @@ echo "[
 
 	{'crawler': 'blogger', 'params': {
 		'email'   : 'nenegoro@gmail.com',
-		'password': 'zinaida123097',
+		'password': '',
 		'blogid'  : '1160797566605666733',
 		'body'    : 'какой-то текст, тестатест',
 		'tags'    : 'тэг, еще, третий',
